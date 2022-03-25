7308991c454072d64e752a9fad740396c3f89c55
WebMapDesign <101799669+WebMapDesign@users.noreply.github.com>
2022-03-25 17:41:39 +0200

    Add files via upload

--- scripts/cities.js
+++ scripts/cities.js
@@ -3,15 +3,15 @@ const geojsonCities = {
 "name": "cities",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "name": "Mutare" }, "geometry": { "type": "Point", "coordinates": [ 32.644198, -18.982587 ] } },
-{ "type": "Feature", "properties": { "name": "Mavingo" }, "geometry": { "type": "Point", "coordinates": [ 30.83053113619275, -20.078622244419744 ] } },
-{ "type": "Feature", "properties": { "name": "Gwanda" }, "geometry": { "type": "Point", "coordinates": [ 29.028331371177099, -20.927165930174727 ] } },
-{ "type": "Feature", "properties": { "name": "Bulawayo" }, "geometry": { "type": "Point", "coordinates": [ 28.562014570897333, -20.132134008386274 ] } },
-{ "type": "Feature", "properties": { "name": "Lupane" }, "geometry": { "type": "Point", "coordinates": [ 27.763160380254124, -18.920474781429832 ] } },
-{ "type": "Feature", "properties": { "name": "Gweru" }, "geometry": { "type": "Point", "coordinates": [ 29.819541024110801, -19.467059227659394 ] } },
-{ "type": "Feature", "properties": { "name": "Marondera" }, "geometry": { "type": "Point", "coordinates": [ 31.554851084168291, -18.18659916131741 ] } },
-{ "type": "Feature", "properties": { "name": "Harare" }, "geometry": { "type": "Point", "coordinates": [ 31.054133864195755, -17.823483620115951 ] } },
-{ "type": "Feature", "properties": { "name": "Bindura" }, "geometry": { "type": "Point", "coordinates": [ 31.317870415173655, -17.309388459151787 ] } },
-{ "type": "Feature", "properties": { "name": "Chinhoyi" }, "geometry": { "type": "Point", "coordinates": [ 30.15398954890162, -17.362900223118313 ] } }
+{ "type": "Feature", "properties": { "name": "Mutare", "position": "left" }, "geometry": { "type": "Point", "coordinates": [ 32.644198, -18.982587 ] } },
+{ "type": "Feature", "properties": { "name": "Mavingo", "position": "left" }, "geometry": { "type": "Point", "coordinates": [ 30.83053113619275, -20.078622244419744 ] } },
+{ "type": "Feature", "properties": { "name": "Gwanda", "position": "left" }, "geometry": { "type": "Point", "coordinates": [ 29.028331371177099, -20.927165930174727 ] } },
+{ "type": "Feature", "properties": { "name": "Bulawayo", "position": "left" }, "geometry": { "type": "Point", "coordinates": [ 28.562014570897333, -20.132134008386274 ] } },
+{ "type": "Feature", "properties": { "name": "Lupane", "position": "left" }, "geometry": { "type": "Point", "coordinates": [ 27.763160380254124, -18.920474781429832 ] } },
+{ "type": "Feature", "properties": { "name": "Gweru", "position": "left" }, "geometry": { "type": "Point", "coordinates": [ 29.819541024110801, -19.467059227659394 ] } },
+{ "type": "Feature", "properties": { "name": "Marondera", "position": "left" }, "geometry": { "type": "Point", "coordinates": [ 31.554851084168291, -18.18659916131741 ] } },
+{ "type": "Feature", "properties": { "name": "Harare", "position": "left" }, "geometry": { "type": "Point", "coordinates": [ 31.054133864195755, -17.823483620115951 ] } },
+{ "type": "Feature", "properties": { "name": "Bindura", "position": "right" }, "geometry": { "type": "Point", "coordinates": [ 31.317870415173655, -17.309388459151787 ] } },
+{ "type": "Feature", "properties": { "name": "Chinhoyi", "position": "left" }, "geometry": { "type": "Point", "coordinates": [ 30.15398954890162, -17.362900223118313 ] } }
 ]
 };
---
+++ scripts/text_province_names.js
@@ -0,0 +1,17 @@
+const geojsonProvinceCentroids = {
+"type": "FeatureCollection",
+"name": "admin_1_province_centroids",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ADM1_EN": "Bulawayo", "ADM1_PCODE": "ZW10" }, "geometry": { "type": "Point", "coordinates": [ 28.55, -19.82 ] } },
+{ "type": "Feature", "properties": { "ADM1_EN": "Harare", "ADM1_PCODE": "ZW19" }, "geometry": { "type": "Point", "coordinates": [ 31.55, -17.85 ] } },
+{ "type": "Feature", "properties": { "ADM1_EN": "Manicaland", "ADM1_PCODE": "ZW11" }, "geometry": { "type": "Point", "coordinates": [ 32.10, -19.35 ] } },
+{ "type": "Feature", "properties": { "ADM1_EN": "Mashonaland Central", "ADM1_PCODE": "ZW12" }, "geometry": { "type": "Point", "coordinates": [ 31.25, -16.53 ] } },
+{ "type": "Feature", "properties": { "ADM1_EN": "Mashonaland East", "ADM1_PCODE": "ZW13" }, "geometry": { "type": "Point", "coordinates": [ 31.00, -18.40 ] } },
+{ "type": "Feature", "properties": { "ADM1_EN": "Mashonaland West", "ADM1_PCODE": "ZW14" }, "geometry": { "type": "Point", "coordinates": [ 29.70, -17.00 ] } },
+{ "type": "Feature", "properties": { "ADM1_EN": "Masvingo", "ADM1_PCODE": "ZW18" }, "geometry": { "type": "Point", "coordinates": [ 31.15, -21.80 ] } },
+{ "type": "Feature", "properties": { "ADM1_EN": "Matabeleland North", "ADM1_PCODE": "ZW15" }, "geometry": { "type": "Point", "coordinates": [ 26.85, -19.25 ] } },
+{ "type": "Feature", "properties": { "ADM1_EN": "Matabeleland South", "ADM1_PCODE": "ZW16" }, "geometry": { "type": "Point", "coordinates": [ 28.75, -21.28 ] } },
+{ "type": "Feature", "properties": { "ADM1_EN": "Midlands", "ADM1_PCODE": "ZW17" }, "geometry": { "type": "Point", "coordinates": [ 30.36, -19.65 ] } }
+]
+};
