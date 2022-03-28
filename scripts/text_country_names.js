6d8ad9512a8d28b657e483870d817fdbaf82f72b
WebMapDesign <101799669+WebMapDesign@users.noreply.github.com>
2022-03-28 09:20:29 +0300

    Update text_country_names.js

--- scripts/text_country_names.js
+++ scripts/text_country_names.js
@@ -3,10 +3,6 @@ const geojsonCountryNames = {
 "name": "text_country_names",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "name": "Zimbabwe" }, "geometry": { "type": "Point", "coordinates": [ 29.90, -17.70 ] } },
-{ "type": "Feature", "properties": { "name": "Botswana" }, "geometry": { "type": "Point", "coordinates": [ 26.20, -20.85 ] } },
-{ "type": "Feature", "properties": { "name": "Mozambique" }, "geometry": { "type": "Point", "coordinates": [ 31.91, -15.38 ] } },
-{ "type": "Feature", "properties": { "name": "Zambia" }, "geometry": { "type": "Point", "coordinates": [ 26.22, -16.28 ] } },
-{ "type": "Feature", "properties": { "name": "South Africa" }, "geometry": { "type": "Point", "coordinates": [ 29.79, -22.75 ] } }
+{ "type": "Feature", "properties": { "name": "Zimbabwe" }, "geometry": { "type": "Point", "coordinates": [ 29.90, -17.70 ] } }
 ]
 }
